266840a1eba0c405ea1573456d243703696be6df
dinhhuychuong3005 <dinhhuychuong3005@gmail.com>
2021-06-08 22:35:17 +0700

    Bt_FacadePattern

---
+++ src/btTrienKhaiFacadePattern/Client.java
@@ -0,0 +1,8 @@
+package btTrienKhaiFacadePattern;
+
+public class Client {
+    public static void main(String[] args) {
+        RandomListFacade randomListFacade = new RandomListFacade(50,5,100);
+        randomListFacade.printRandomEvenList();
+    }
+}
---
+++ src/btTrienKhaiFacadePattern/RandomListFacade.java
@@ -0,0 +1,29 @@
+package btTrienKhaiFacadePattern;
+
+import btTrienKhaiFacadePattern.ListFilter;
+import btTrienKhaiFacadePattern.ListPrinter;
+import btTrienKhaiFacadePattern.RandomList;
+
+import java.util.List;
+
+public class RandomListFacade {
+    private ListFilter listFilter;
+    private ListPrinter listPrinter;
+    private RandomList randomList;
+    private int max, min, size;
+
+    public RandomListFacade(int max, int min, int size) {
+        listFilter = new ListFilter();
+        listPrinter = new ListPrinter();
+        randomList = new RandomList();
+        this.max = max;
+        this.min = min;
+        this.size = size;
+    }
+
+    public void printRandomEvenList(){
+        List<Integer> listInteger = randomList.generateList(size, min, max);
+        List<Integer> listEven = listFilter.filterOdd(listInteger);
+        listPrinter.printList(listEven);
+    }
+}
