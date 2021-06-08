13b95928c60cefa61ca27e27fb073cd76c5a6569
dinhhuychuong3005 <dinhhuychuong3005@gmail.com>
2021-06-08 22:46:53 +0700

    BT_ApdungAdapter

---
+++ src/Bt_ApdungAdapter/CollectionOperations.java
@@ -0,0 +1,7 @@
+package Bt_ApdungAdapter;
+
+import java.util.Set;
+
+public interface CollectionOperations {
+    public int findMax(Set<Integer> numbers);
+}
---
+++ src/Bt_ApdungAdapter/Main.java
@@ -0,0 +1,18 @@
+package Bt_ApdungAdapter;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) {
+        Set<Integer> number = new HashSet<>();
+        number.add(2);
+        number.add(6);
+        number.add(7);
+        number.add(8);
+
+        CollectionUtilsAdapter collectionOperations = new CollectionUtilsAdapter();
+        Client client = new Client(collectionOperations);
+        client.printMaxValue(number);
+    }
+}
